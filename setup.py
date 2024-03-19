c5ebbe3122bb42b74a96ae2fecd00aaf954b6e2a
David Northcote <42978178+dnorthcote@users.noreply.github.com>
2024-03-19 22:59:59 +0000

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,37 +1,68 @@
-# library
 import os
-from setuptools import find_packages, setup
 from distutils.dir_util import copy_tree
+from setuptools import find_packages, setup
 
-# variable
-package_name = "pynq_sobel"
-board = os.environ['BOARD']
-board_notebooks = os.path.join(os.environ['PYNQ_JUPYTER_NOTEBOOKS'], 'pynq_sobel')
+class package_installer():
+    def __init__(self,
+                 name,
+                 version,
+                 description,
+                 author,
+                 email,
+                 license,
+                 url,
+                 pynq_version,
+                 board):
+        
+        args = locals()
+        for key in args:
+            if key != 'self':
+                setattr(self, key, args[key])
+        self.check_board()
+        self.copy_projects()
+        self.run_setup()
 
-# function
-def copy_bitstreams(brd, pkgname):
-    for directory in os.walk(os.path.join(os.getcwd(), 'boards', brd)):
-        for file in directory[2]:
-            if (file.find('.bit') != -1):
-                project = os.path.split(os.path.split(directory[0])[0])[1]
-                dst = os.path.join(os.getcwd(), pkgname, 'notebooks', project, 'bitstream')
-                copy_tree(directory[0], dst)
+    def check_board(self):
+        if not os.path.isdir(f'boards/{self.board}/{self.name}'):
+            raise ValueError("Board {} is not supported.".format(self.board))
+        if not os.path.isdir(os.environ['PYNQ_JUPYTER_NOTEBOOKS']):
+            raise ValueError(
+                "Directory {} does not exist.".format(os.environ['PYNQ_JUPYTER_NOTEBOOKS']))
+        
+    def copy_projects(self):
+        cwd = os.getcwd()
+        for prj in next(os.walk(os.path.join(cwd, 'boards', self.board)))[1]:
+            temp_prj = os.path.join(cwd, 'boards', self.board, prj)
+            for directory in next(os.walk(temp_prj))[1]:
+                src = os.path.join(temp_prj, directory)
+                dst = os.path.join(cwd, self.name, prj, directory)
+                copy_tree(src, dst)
 
-def generate_pkg_dirs(pkgname):
-    data_files = []
-    for directory in os.walk(os.path.join(os.getcwd(), pkgname)):
-        for file in directory[2]:
-            data_files.append("".join([directory[0],"/",file]))
-    return data_files
+    def generate_pkg_dirs(self):
+        data_files = []
+        for directory in os.walk(os.path.join(os.getcwd(), self.name)):
+            for file in directory[2]:
+                data_files.append("".join([directory[0],"/",file]))
+        return data_files
 
-copy_bitstreams(board, package_name)
+    def run_setup(self):
+        setup(name=self.name,
+              version=self.version,
+              install_requires=[self.pynq_version],
+              url=self.url,
+              license=self.license,
+              author=self.author,
+              author_email=self.email,
+              packages=find_packages(),
+              package_data={'': self.generate_pkg_dirs()},
+              description=self.description)
 
-# setuptools
-setup(
-    name=package_name,
-    version='1.0',
-    install_requires=[],
-    author="strath-sdr",
-    packages=find_packages(),
-    package_data={"" : generate_pkg_dirs(package_name)},
-    description="Hardware Accelerated Sobel Filter for PYNQ.")
+package_installer(name = "pynq_sobel",
+                  version = "1.0.0",
+                  description  = "PYNQ Sobel Filter.",
+                  author = "David Northcote",
+                  email = "david.northcote@strath.ac.uk",
+                  license = "BSD 3-Clause License",
+                  url = "https://github.com/strath-sdr/pynq_sobel.git",
+                  pynq_version = "pynq==2.7",
+                  board = os.environ['BOARD'])
